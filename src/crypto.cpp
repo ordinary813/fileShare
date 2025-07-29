d61f080940ff0813ef7a55ee161e504e79520236
ordinary813 <ordinaryperson813@gmail.com>
2025-07-30 01:00:15 +0300

    Updated to openssl evp interface

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,8 +1,9 @@
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <iomanip>
+#include <sstream>
 
 namespace p2pfs
 {
@@ -12,21 +13,39 @@ namespace p2pfs
         if (!file)
             return "";
 
-        SHA256_CTX ctx;
-        SHA256_Init(&ctx);
+        EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+        if (!ctx)
+            return "";
+
+        if (EVP_DigestInit_ex(ctx, EVP_sha256(), nullptr) != 1)
+        {
+            EVP_MD_CTX_free(ctx);
+            return "";
+        }
 
         std::vector<char> buffer(8192);
         while (file.read(buffer.data(), buffer.size()) || file.gcount())
         {
-            SHA256_Update(&ctx, buffer.data(), file.gcount());
+            if (EVP_DigestUpdate(ctx, buffer.data(), file.gcount()) != 1)
+            {
+                EVP_MD_CTX_free(ctx);
+                return "";
+            }
         }
 
-        unsigned char hash[SHA256_DIGEST_LENGTH];
-        SHA256_Final(hash, &ctx);
+        unsigned char hash[EVP_MAX_MD_SIZE];
+        unsigned int hash_len = 0;
+        if (EVP_DigestFinal_ex(ctx, hash, &hash_len) != 1)
+        {
+            EVP_MD_CTX_free(ctx);
+            return "";
+        }
+
+        EVP_MD_CTX_free(ctx);
 
         std::ostringstream result;
-        for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
-            result << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+        for (unsigned int i = 0; i < hash_len; ++i)
+            result << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
 
         return result.str();
     }
